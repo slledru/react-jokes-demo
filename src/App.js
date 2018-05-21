5a9a315f36662ba6d93f027bcafe5ece3055a644
slledru <slledru@msn.com>
2018-05-21 16:36:50 -0600

    adding joke using redux/thunk

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Jokes from './components/Jokes'
+import Jokes from './containers/Jokes'
 
 import AddJokeForm from './containers/AddJokeForm'
 
-// normally this comes from process.env.API
-const API = 'https://powerful-beyond-70119.herokuapp.com/jokes'
-
 class App extends Component {
-  // set state here
-  constructor(props) {
-    super(props)
-    this.state = { jokes: [] }
-  }
-
   async componentDidMount() {
     // const response = await fetch(API)
     // const json = await response.json()
@@ -22,25 +13,11 @@ class App extends Component {
     //   jokes: json })
   }
 
-  addJoke = async (joke) => {
-    console.log("joke to send to API", joke)
-    const response = await fetch(API, {
-      method: 'POST',
-      body: JSON.stringify(joke),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-    const newJoke = await response.json()
-    this.setState({jokes: [...this.state.jokes, newJoke]})
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Jokes</h1>
-        <Jokes jokes={ this.state.jokes } title="World's best Penguin jokes"/>
+        <Jokes />
         <AddJokeForm />
       </div>
     );
